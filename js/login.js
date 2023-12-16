78acfe28f17f9e041adaca09ebabcf79f77c688b
crystal <parkfam044@naver.com>
2023-12-16 17:28:50 +0900

    회원가입 후 새로운 사람 로그인 안 됨

--- js/login.js
+++ js/login.js
@@ -1,23 +1,26 @@
 // localStorage.clear();
 
+
 const users = [
   {name: '관리자', id: 'admin', password: 'admin'},
   {name: '행복', id: 'happy', password: 'call'},
   {name: '행운', id: 'luck', password: 'key'},
   {name: '안녕', id: 'how', password: 'areyou'}
-
 ]
-
 const usersJSON = JSON.stringify(users)
 
 localStorage.setItem('users', usersJSON);
-const storedUsersJSON = localStorage.getItem('users');
+
+
+
+
+// const storedUsersJSON = localStorage.getItem('users');
 
 // Parse the JSON string back to an array
-const storedUsers = JSON.parse(storedUsersJSON);
+// const storedUsers = JSON.parse(storedUsersJSON);
 
 // Log the stored users to the console
-console.log(storedUsers);
+// console.log(storedUsers);
 
 document.getElementById('login-form').addEventListener('submit', function(e) {
   e.preventDefault(); // 폼 제출 기본 동작 막기
@@ -39,11 +42,11 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
 
 
   // 입력한 아이디와 비밀번호가 Local Storage에 저장된 데이터와 일치하는지 확인
-  if (username === savedUsername && password === savedPassword) {
-    alert('로그인 성공');
-  } else {
-    alert('아이디 또는 비밀번호를 확인해주세요');
-  }
+  // if (username === savedUsername && password === savedPassword) {
+  //   alert('로그인 성공');
+  // } else {
+  //   alert('아이디 또는 비밀번호를 확인해주세요');
+  // }
 });
 
 document.getElementById('register-form').addEventListener('submit', function(e) {
@@ -58,21 +61,19 @@ document.getElementById('register-form').addEventListener('submit', function(e) 
   // localStorage.setItem('username', newUsername);
   // localStorage.setItem('password', newPassword);
 
-  const newUsers = {name: newname, id: newUsername, password: newPassword}
-  ;
+  const newUsers = {name: newname, id: newUsername, password: newPassword};
 
   users.push(newUsers);
-
   const usersJSON = JSON.stringify(users)
 
   localStorage.setItem('users', usersJSON);
-  const storedUsersJSON = localStorage.getItem('users');
+  // const storedUsersJSON = localStorage.getItem('users');
 
-  // Parse the JSON string back to an array
-  const storedUsers = JSON.parse(storedUsersJSON);
+  // // Parse the JSON string back to an array
+  // const storedUsers = JSON.parse(storedUsersJSON);
 
-  // Log the stored users to the console
-  console.log(storedUsers);
+  // // Log the stored users to the console
+  // console.log(storedUsers);
 
   alert('회원가입 성공');
 });
